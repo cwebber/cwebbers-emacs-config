2b79ddb51d023a4928acf27bd77296f2bb2c3e5b
Christopher Allan Webber <cwebber@dustycloud.org>
2010-02-25 10:09:06 -0600

    Added sudo for creativecommons.org to the list of default-proxies-alist

--- modes/tramp.el
+++ modes/tramp.el
@@ -5,3 +5,5 @@
 (setq tramp-default-method "ssh")
 (add-to-list 'tramp-default-method-alist '("localhost" "" "sudo"))
 (setq tramp-backup-directory-alist backup-directory-alist)
+(add-to-list 'tramp-default-proxies-alist
+             '("\\.creativecommons\\.org\\'" "\\`root\\'" "/ssh:%h:"))
