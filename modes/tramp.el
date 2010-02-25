fa8531f200dd56621a19f30661eed581101bd300
Christopher Allan Webber <cwebber@dustycloud.org>
2010-02-25 10:10:04 -0600

    Make that sudo: connection universal

--- modes/tramp.el
+++ modes/tramp.el
@@ -6,4 +6,4 @@
 (add-to-list 'tramp-default-method-alist '("localhost" "" "sudo"))
 (setq tramp-backup-directory-alist backup-directory-alist)
 (add-to-list 'tramp-default-proxies-alist
-             '("\\.creativecommons\\.org\\'" "\\`root\\'" "/ssh:%h:"))
+             '(nil "\\`root\\'" "/ssh:%h:"))
