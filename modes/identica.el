4177fdf2a2b7ab3bd63b0f25629541be4aedcb71
Christopher Allan Webber <cwebber@dustycloud.org>
2010-06-11 09:33:06 -0500

    Added identica-modeto the modes directory

---
+++ modes/identica.el
@@ -0,0 +1,7 @@
+(load-file "~/elisp/identica-mode/identica-mode.el")
+(require 'identica-mode)
+
+(setq identica-update-status-method 'edit-buffer)
+
+; We'll set the username here but NOT our password.
+(setq identica-username "cwebber")
