480d10b527b22c70cd9bfc2e8b92fc7939b256aa
Christopher Allan Webber <cwebber@dustycloud.org>
2010-11-14 13:09:00 -0600

    Adding uniquify.  Not sure it replaces my own buffer-uniquing stuff though..

---
+++ modes/uniquify.el
@@ -0,0 +1,8 @@
+;; Use Uniquify to keep buffer names unique and meaninful
+;; (thanks emacswiki!)
+
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'reverse)
+(setq uniquify-separator "/")
+(setq uniquify-after-kill-buffer-p t) ; rename after killing uniquified
+(setq uniquify-ignore-buffers-re "^\\*") ; don't muck with special buffers
