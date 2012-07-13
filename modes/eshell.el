70c92faac2792a561d65db694fb340cb501d80e5
Christopher Allan Webber <cwebber@dustycloud.org>
2012-07-13 16:28:52 -0500

    unset pager in eshell

--- modes/eshell.el
+++ modes/eshell.el
@@ -19,7 +19,22 @@
 
 (defun eshell-add-scroll-to-bottom ()
   (interactive)
-  (make-local-hook 'window-scroll-functions)
+  ;(make-local-hook 'window-scroll-functions)
   (add-hook 'window-scroll-functions 'eshell-scroll-to-bottom nil t))
 
 (add-hook 'eshell-mode-hook 'eshell-add-scroll-to-bottom)
+
+; Set the $PAGER to ""
+
+(defun eshell-unset-pager ()
+  (setenv "PAGER" ""))
+
+(add-hook 'eshell-mode-hook 'eshell-unset-pager)
+
+; another emacswiki borrow
+
+;; (require 'ansi-color)
+;; (defun eshell-handle-ansi-color ()
+;;   (ansi-color-apply-on-region eshell-last-output-start
+;;                               eshell-last-output-end))
+;; (add-to-list 'eshell-output-filter-functions 'eshell-handle-ansi-color)
