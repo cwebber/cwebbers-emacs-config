80b3cd31830c1d10b470c0338a43d6e7d7886ab0
Christopher Allan Webber <cwebber@dustycloud.org>
2012-07-31 20:14:33 -0500

    More mu4e stuffs

--- modes/mu4e.el
+++ modes/mu4e.el
@@ -2,6 +2,10 @@
       mu4e-sent-folder   "/sent-to-misc"       ;; where do i keep sent mail?
       mu4e-drafts-folder "/drafts")     ;; where do i keep half-written mail?
       ; mu4e-trash-folder  "/trash")     ;; where do i move deleted mail?
-(add-to-list 'load-path "~/programs/maildir-utils/emacs/")
-(setq mu4e-mu-binary "/home/cwebber/programs/maildir-utils/mu/mu")
+(add-to-list 'load-path "~/programs/mu/mu4e/")
+(setq mu4e-mu-binary "/home/cwebber/programs/mu/mu/mu")
+
+(setq mu4e-get-mail-command "offlineimap")
+
 (require 'mu4e)
+(require 'org-mu4e)
