135590eebf4c8314c7e7799031cb2e441bd261f2
Christopher Allan Webber <cwebber@dustycloud.org>
2013-04-21 12:14:25 -0500

    Replaced pager-set-to-nothing with ~/.emacs_bash

--- modes/shell.el
+++ modes/shell.el
@@ -1,4 +1,1 @@
-(defun shell-set-pager-nothing ()
-  (comint-send-string (get-buffer-process (current-buffer)) "export PAGER=\"\"")
-  (comint-send-string (get-buffer-process (current-buffer)) "\n"))
-    
+; Replaced pager-set-to-nothing with ~/.emacs_bash
