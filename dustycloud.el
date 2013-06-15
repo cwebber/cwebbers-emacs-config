4be934ac96eea0dd46ba9b2e658136a0ba965271
Christopher Allan Webber <cwebber@dustycloud.org>
2013-06-15 18:18:00 -0500

    dustycloud.el... maybe should be blogging-tools.el, whatever.

---
+++ dustycloud.el
@@ -0,0 +1,43 @@
+(setq dustycloud-content-dir "~/devel/dustycloud/content/")
+
+(setq dustycloud-rst-snippet "${1:$(make-string (string-width text) ?\=)}
+${1:Title}
+${1:$(make-string (string-width text) ?\=)}
+
+:date: `(format-time-string \"\%Y-\%m-\%d \%H:\%M\")`
+:author: ${2:Christopher Allan Webber}
+:tags: $3
+
+$0")
+
+(setq dustycloud-html-snippet "<html>
+<head>
+  <title>$1</title>
+  <meta name=\"date\" contents=\"`(format-time-string \"\%Y-\%m-\%d \%H:\%M\")`\" />
+  <meta name=\"author\" contents=\"${2:Christopher Allan Webber}\" />
+  <meta name=\"tags\" contents=\"$3\" />
+</head>
+<body>
+$0
+</body>
+</html>
+")
+
+
+(defun dustycloud-compose ()
+  "Compose a new document for the dustycloud blog"
+  (interactive)
+  (let ((default-directory dustycloud-content-dir))
+    (call-interactively 'find-file))
+  (let ((file-extension
+         (progn (string-match "\\.\\(html\\|rst\\)$" buffer-file-name)
+                (match-string 1 buffer-file-name)))
+        (yas/indent-line 'fixed)
+        (yas/wrap-around-region nil))
+    ;; Expand the appropriate snippet (or throw an error if unrecognized)
+    (cond ((equal file-extension "rst")
+           (yas-expand-snippet dustycloud-rst-snippet))
+          ((equal file-extension "html")
+           (yas-expand-snippet dustycloud-html-snippet))
+          (t
+           (error "File extension not .rst or .html")))))
