da554ccef1ef0d5e8e4970be33610e5d0af40713
Christopher Allan Webber <cwebber@dustycloud.org>
2013-11-24 20:46:57 -0600

    Use naquadah-themed parens!

--- modes/highlight-parentheses.el
+++ modes/highlight-parentheses.el
@@ -1,3 +1,16 @@
 (require 'highlight-parentheses)
 
 (add-hook 'emacs-lisp-mode-hook 'highlight-parentheses-mode)
+
+; Use naquadah colors
+(setq hl-paren-colors
+      (list
+       (naquadah-get-colors 'gradient-1)
+       (naquadah-get-colors 'gradient-2)
+       (naquadah-get-colors 'gradient-3)
+       (naquadah-get-colors 'gradient-4)
+       (naquadah-get-colors 'gradient-5)
+       (naquadah-get-colors 'gradient-6)
+       (naquadah-get-colors 'gradient-7)
+       (naquadah-get-colors 'gradient-8)))
+
