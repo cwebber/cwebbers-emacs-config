28741739e160acdb6a5d4af22ac23395c06b8337
Christopher Allan Webber <cwebber@dustycloud.org>
2013-11-24 20:35:36 -0600

    Make highlight-sexps more usable/readable: make the inner part darker!

--- modes/highlight-sexps.el
+++ modes/highlight-sexps.el
@@ -1,5 +1,5 @@
 ; (load-file "~/elisp/highlight-sexps.el")
 
-(setq hl-sexp-background-colors '("#445255" "#353d3f"))
+(setq hl-sexp-background-colors '("#201e1b" "#2b2925"))
 (add-hook 'lisp-mode-hook 'highlight-sexps-mode)
 (add-hook 'emacs-lisp-mode-hook 'highlight-sexps-mode)
