f9933e707fa86a8879edd7d8fb66b3891637ba45
Christopher Allan Webber <cwebber@dustycloud.org>
2014-09-03 08:52:36 -0500

    No longer load mu4e from the programs directory

--- modes/mu4e.el
+++ modes/mu4e.el
@@ -2,8 +2,8 @@
       mu4e-sent-folder   "/sent-to-misc"       ;; where do i keep sent mail?
       mu4e-drafts-folder "/drafts")     ;; where do i keep half-written mail?
       ; mu4e-trash-folder  "/trash")     ;; where do i move deleted mail?
-(add-to-list 'load-path "~/programs/mu/mu4e/")
-(setq mu4e-mu-binary "/home/cwebber/programs/mu/mu/mu")
+; (add-to-list 'load-path "~/programs/mu/mu4e/")
+; (setq mu4e-mu-binary "/home/cwebber/programs/mu/mu/mu")
 
 (setq mu4e-get-mail-command "offlineimap")
 (setq mu4e-html2text-command "html2text -utf8 -width 72")
