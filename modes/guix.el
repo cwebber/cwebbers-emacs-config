a5af14d51b874368c2fa9d54d821ab4533be7e6c
Christopher Allan Webber <cwebber@dustycloud.org>
2015-04-20 19:54:42 -0500

    Now that's a nicer way to launch guile for guix

--- modes/guix.el
+++ modes/guix.el
@@ -1,11 +1,11 @@
 (add-to-list 'load-path "/home/cwebber/devel/guix/emacs")
 
-;; Not sure this helps :(
-(setenv "GUILE_LOAD_PATH" "/home/cwebber/devel/guix")
-(setenv "GUILE_LOAD_COMPILED_PATH" "/home/cwebber/devel/guix")
-
 (setq guix-helper-file "/home/cwebber/devel/guix/emacs/guix-helper.scm")
 
 (setq guix-load-path "/home/cwebber/devel/guix/emacs")
 
+(setq guix-guile-program '("/home/cwebber/devel/guix/pre-inst-env" "guile"))
+
+(require 'guix-autoloads)
 (require 'guix-init)
+
