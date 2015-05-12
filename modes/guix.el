b95c78ea1075f6e30b50cf88bbee9f5f48c482b9
Christopher Allan Webber <cwebber@dustycloud.org>
2015-05-12 12:34:36 -0500

    cutting down what's needed / isn't in the guix stuff

--- modes/guix.el
+++ modes/guix.el
@@ -1,11 +1,8 @@
 (add-to-list 'load-path "/home/cwebber/devel/guix/emacs")
 
-(setq guix-helper-file "/home/cwebber/devel/guix/emacs/guix-helper.scm")
-
 (setq guix-load-path "/home/cwebber/devel/guix/emacs")
 
 (setq guix-guile-program '("/home/cwebber/devel/guix/pre-inst-env" "guile"))
 
-(require 'guix-autoloads)
 (require 'guix-init)
 
