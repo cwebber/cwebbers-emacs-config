56b15e3c327455a6f3d98314e7f53a0da89ebda3
Christopher Allan Webber <cwebber@dustycloud.org>
2015-08-31 21:07:25 -0500

    rainbow-identifiers is arguably kind of interesting

---
+++ modes/rainbow-identifiers.el
@@ -0,0 +1,7 @@
+(require 'rainbow-identifiers)
+
+(add-hook 'emacs-lisp-mode-hook 'rainbow-identifiers-mode)
+(add-hook 'hy-mode-hook 'rainbow-identifiers-mode)
+(add-hook 'inferior-lisp-mode-hook 'rainbow-identifiers-mode)
+(add-hook 'scheme-mode-hook 'rainbow-identifiers-mode)
+(add-hook 'lisp-mode-hook 'rainbow-identifiers-mode)
