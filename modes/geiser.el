922df331041ed650544b3e7b8dc7e553f57c1555
Christopher Allan Webber <cwebber@dustycloud.org>
2015-09-03 10:55:28 -0500

    correct way to enable/disable modes

--- modes/geiser.el
+++ modes/geiser.el
@@ -3,8 +3,8 @@
 (setq geiser-mode-smart-tab-p t)
 
 (defun cwebber/geiser-completion-prefs ()
-  (setq auto-complete-mode nil)
-  (setq company-mode t))
+  (auto-complete-mode -1)
+  (company-mode 1))
 
 (add-hook 'geiser-mode-hook 'cwebber/geiser-completion-prefs)
 (add-hook 'geiser-repl-mode-hook 'cwebber/geiser-completion-prefs)
