b34149831da7789e95a761e491d0baceb226572b
Christopher Allan Webber <cwebber@dustycloud.org>
2015-09-24 14:38:51 -0500

    helm.el

---
+++ modes/helm.el
@@ -0,0 +1,13 @@
+(require 'helm)
+(require 'helm-pages)
+
+(global-set-key (kbd "C-x B") 'helm-buffers-list)
+(global-set-key (kbd "M-s O") 'helm-occur)
+(global-set-key (kbd "M-X") 'helm-M-x)
+(global-set-key (kbd "C-c f h") 'helm-find-files)
+
+(global-set-key (kbd "C-c h b") 'helm-buffers-list)
+(global-set-key (kbd "C-c h f") 'helm-find-files)
+(global-set-key (kbd "C-c h F") 'helm-find)
+(global-set-key (kbd "C-c h o") 'helm-occur)
+(global-set-key (kbd "C-c h x") 'helm-M-x)
