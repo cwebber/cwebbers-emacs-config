17f2da2b4e5890836a4a219dfda62b1895cf9b3c
Christopher Allan Webber <cwebber@dustycloud.org>
2015-10-10 14:05:21 -0500

    helm-pages key

--- modes/helm.el
+++ modes/helm.el
@@ -10,4 +10,5 @@
 (global-set-key (kbd "C-c h f") 'helm-find-files)
 (global-set-key (kbd "C-c h F") 'helm-find)
 (global-set-key (kbd "C-c h o") 'helm-occur)
+(global-set-key (kbd "C-c h p") 'helm-pages)
 (global-set-key (kbd "C-c h x") 'helm-M-x)
