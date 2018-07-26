53eb534aad1128e25fa1ca40b61d8de540f1672c
Christopher Lemmer Webber <cwebber@dustycloud.org>
2018-07-26 12:39:36 -0400

    Add racket-mode to more common mode hooks

--- modes/rainbow-delimiters.el
+++ modes/rainbow-delimiters.el
@@ -5,5 +5,6 @@
 (add-hook 'inferior-lisp-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'scheme-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'lisp-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'racket-mode-hook 'rainbow-identifiers-mode)
 
 (setq rainbow-delimiters-max-face-count 11)
--- modes/rainbow-identifiers.el
+++ modes/rainbow-identifiers.el
@@ -5,3 +5,4 @@
 (add-hook 'inferior-lisp-mode-hook 'rainbow-identifiers-mode)
 (add-hook 'scheme-mode-hook 'rainbow-identifiers-mode)
 (add-hook 'lisp-mode-hook 'rainbow-identifiers-mode)
+(add-hook 'racket-mode-hook 'rainbow-identifiers-mode)
