a54eb60d387b2ec6182be9a2b9c273e044a56c0e
Christopher Lemmer Webber <cwebber@dustycloud.org>
2018-07-26 12:45:14 -0400

    comment out hardcoded paths in slime

--- modes/slime.el
+++ modes/slime.el
@@ -2,9 +2,9 @@
 ;; Slime mode
 ;; ---------
 
-(setq inferior-lisp-program "/usr/bin/sbcl")
-(add-to-list 'load-path "/usr/share/emacs23/site-lisp/slime/")
-(add-to-list 'load-path "/usr/share/common-lisp/source/slime/")
-(setq slime-backend "/usr/share/common-lisp/source/slime/swank-loader.lisp")
+;; (setq inferior-lisp-program "/usr/bin/sbcl")
+;; (add-to-list 'load-path "/usr/share/emacs23/site-lisp/slime/")
+;; (add-to-list 'load-path "/usr/share/common-lisp/source/slime/")
+;; (setq slime-backend "/usr/share/common-lisp/source/slime/swank-loader.lisp")
 (require 'slime)
 (slime-setup '(slime-repl))
