4f68c17acfc1000695b2d676c03c3babe461a09a
Christopher Lemmer Webber <cwebber@dustycloud.org>
2020-11-15 13:13:42 -0500

    dimish foo

--- modes/diminish.el
+++ modes/diminish.el
@@ -5,3 +5,5 @@
 (diminish 'highlight-parentheses-mode)
 (diminish 'smartparens-mode)
 (diminish 'auto-complete-mode)
+(diminish 'auto-revert-mode)
+(diminish 'hs-minor-mode)
--- modes/racket-mode.el
+++ modes/racket-mode.el
@@ -3,3 +3,5 @@
 
 ;;; wtf why isn't it working
 ;; (add-hook ’racket-mode-hook ’racket-xp-mode)
+
+(put 'let-on 'racket-indent-function 1)
