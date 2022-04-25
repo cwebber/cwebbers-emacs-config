1d32d4d82f0622a690fc577a8c5fe219c3a70f48
Christine Lemmer-Webber <cwebber@dustycloud.org>
2022-04-25 11:52:11 -0400

    put extend-methods as scheme-indent-function 1

--- modes/scheme.el
+++ modes/scheme.el
@@ -3,7 +3,7 @@
 (put 'signal-let* 'scheme-indent-function 1)
 (put 'signal-let 'scheme-indent-function 1)
 (put 'let/cc 'scheme-indent-function 1)
-
+(put 'extend-methods 'scheme-indent-function 1)
 
 ;;; =============
 ;;; From Mark Weaver. See this thread on guix-devel:
