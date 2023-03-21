15f3b3642b46fc8583a9032f0d7f2cf088e9c618
Christine Lemmer-Webber <cwebber@dustycloud.org>
2023-03-21 11:51:13 -0400

    more mu4e config
    even most of this h as moved out of this repo to private stuff

--- modes/mu4e.el
+++ modes/mu4e.el
@@ -1,7 +1,7 @@
 (setq mu4e-maildir       "~/Maildir"   ;; top-level Maildir
-      mu4e-sent-folder   "/sent-to-misc"       ;; where do i keep sent mail?
-      mu4e-drafts-folder "/drafts")     ;; where do i keep half-written mail?
-      ; mu4e-trash-folder  "/trash")     ;; where do i move deleted mail?
+      mu4e-sent-folder   "/personal/sent-to-misc"       ;; where do i keep sent mail?
+      mu4e-drafts-folder "/personal/drafts"     ;; where do i keep half-written mail?
+      mu4e-trash-folder  "/personal/trash")     ;; where do i move deleted mail?
 
 ;; <mark_weaver> paroneayea: it's a hack, but /run/current-system will exist on a
 ;;               GuixSD system and not on Debian.
@@ -17,43 +17,23 @@
 ; (setq mu4e-msg2pdf "/usr/bin/msg2pdf")
 (setq mu4e-msg2pdf "/home/cwebber/programs/mu/toys/msg2pdf/msg2pdf")
 
-(setq mu4e-get-mail-command "fdm fetch -v")
+(setq mu4e-get-mail-command "/home/cwebber/bin/fetch-mail.sh")
 (setq mu4e-html2text-command 'mu4e-shr2text)
 ;; (setq mu4e-html2text-command "html2text -utf8 -width 72")
 ;;;; When all goes badly:
 ;; (setq mu4e-html2text-command "cat")
 
 (setq mu4e-maildir-shortcuts
-      '(("/Inbox.General"     . ?i)
-        ("/CC Staff"   . ?c)
-        ("/Spam"       . ?s)
-        ("/sent-to-misc" . ?S)))
+      '(("/personal/Inbox.General"     . ?i)
+        ("/personal/CC Staff"   . ?c)
+        ("/personal/Spam"       . ?s)
+        ("/personal/sent-to-misc" . ?S)))
 
 (setq smtpmail-queue-mail  nil  ;; start in non-queuing mode
       smtpmail-queue-dir   "~/Maildir/queue/cur")
 
 (setq cwebber/mu4e-important-lists
-      "(maildir:/Inbox.General OR maildir:/Mediagoblin OR \"maildir:/W3C Social WG\" OR \"maildir:/SpritelyBiz Dev Thoughts\")")
-
-(setq mu4e-bookmarks
-  `(("flag:unread AND NOT flag:trashed AND NOT maildir:/Spam" "Unread messages"      ?u)
-    (,(concat "flag:unread AND NOT flag:trashed AND NOT maildir:/Spam AND NOT "
-              cwebber/mu4e-important-lists)
-     "Unread non-important messages"                                                 ?U)
-    ("date:today..now AND NOT maildir:/Spam"                  "Today's messages"     ?T)
-    ;("date:7d..now AND NOT maildir:/Spam"                     "Last 7 days"          ?w)
-    ;("mime:image/* AND NOT maildir:/Spam"                     "Messages with images" ?p)
-    ("flag:unread AND NOT flag:trashed AND maildir:/Spam"     "Unread spam"          ?s)
-    ("flag:unread AND maildir:/Inbox.General"  "Unread general inbox" ?i)
-    (,(concat "flag:unread AND "
-              cwebber/mu4e-important-lists)
-     "Important messages" ?I)
-    ("flag:flagged AND NOT flag:replied" "flagged" ?f)
-    (,(concat "flag:unread AND NOT (flag:flagged) AND"
-              cwebber/mu4e-important-lists)
-     "Un-flagged unread inbox" ?F)
-    ("flag:unread AND maildir:/Mediagoblin"  "MediaGoblin devel" ?m)
-    ("flag:unread AND \"maildir:/MediaGoblin Tickets\""  "Unread general inbox" ?t)))
+      "(maildir:/personal/Inbox.General OR maildir:/personal/Mediagoblin OR \"maildir:/personal/W3C Social WG\")")
 
 (load-file "~/devel/mu4e-uqueue/mu4e-uqueue.el")
 (load-file "~/devel/mu4e-uqueue/uqueue-advice.el")
@@ -83,3 +63,52 @@
 (remove-hook 'mu4e-index-updated-hook 'mu4e~request-contacts)
 
 (setq mu4e-cache-maildir-list t)
+
+;; (setq mu4e-contexts
+;;       (list (make-mu4e-context
+;;              :name "Any")
+;;             (make-mu4e-context
+;;              :name "Personal"
+;;              ;; we match based on the contact-fields of the message
+;;              :enter-func (lambda () (mu4e-message "Personal mail"))
+;;              :match-func (lambda (msg)
+;;                            (when msg
+;;                              (not (string-match-p "^/Spritely" (mu4e-message-field msg :maildir)))))
+;;              :vars '((user-mail-address	    . "cwebber@dustycloud.org")))
+;;             (make-mu4e-context
+;;              :name "Work"
+;;              :enter-func (lambda () (mu4e-message "Work mail"))
+;;              :match-func (lambda (msg)
+;;                            (when msg
+;;                              (mu4e-message-contact-field-matches msg
+;;                                                                  :to "cwebber@dustycloud.org")))
+;;              )
+;;          ,(make-mu4e-context
+;;            :name "Work"
+;;            :enter-func (lambda () (mu4e-message "Switch to the Work context"))
+;;            ;; no leave-func
+;;            ;; we match based on the maildir of the message
+;;            ;; this matches maildir /Arkham and its sub-directories
+;;            :match-func (lambda (msg)
+;;                          (when msg
+;;                            (string-match-p "^/Arkham" (mu4e-message-field msg :maildir))))
+;;            :vars '( ( user-mail-address	     . "aderleth@miskatonic.example.com" )
+;;                     ( user-full-name	     . "Alice Derleth" )
+;;                     ( message-user-organization . "Miskatonic University" )
+;;                     ( mu4e-compose-signature  .
+;;                       (concat
+;;                        "Prof. Alice Derleth\n"
+;;                        "Miskatonic University, Dept. of Occult Sciences\n"))))
+
+;;          ,(make-mu4e-context
+;;            :name "Cycling"
+;;            :enter-func (lambda () (mu4e-message "Switch to the Cycling context"))
+;;            ;; no leave-func
+;;            ;; we match based on the maildir of the message; assume all
+;;            ;; cycling-related messages go into the /cycling maildir
+;;            :match-func (lambda (msg)
+;;                          (when msg
+;;                            (string= (mu4e-message-field msg :maildir) "/cycling")))
+;;            :vars '( ( user-mail-address	     . "aderleth@example.com" )
+;;                     ( user-full-name	     . "AliceD" )
+;;                     ( mu4e-compose-signature  . nil)))))
